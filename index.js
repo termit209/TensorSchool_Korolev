2c44b9ba5fadbae2da5321c408b3ab57747e7e2f
Alexey Korolev <ya.korol209@yandex.ru>
2020-04-27 06:11:45 +0700

    HW1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('HW_1'));
 
 app.listen(3000, function () {
   console.log('Your server listening on port 3000!');
